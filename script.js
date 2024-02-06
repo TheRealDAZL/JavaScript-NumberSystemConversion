7d005189c492b146acafb18ca06b067127dea691
therealdazl <louis.theriault@outlook.fr>
2024-02-06 15:17:37 -0500

    Nouvelle version qui accepte les nombres à virgule (ou à point), version bêta.
    Le code n'est pas encore optimisé.

--- script.js
+++ script.js
@@ -14,27 +14,36 @@ function validertEtConvertir(n, b1, b2) {
     // Note: cette variable sera réutilisée plus tard,
     // vers la fin de la méthode
     let nEstNegatif = n.startsWith("-")
+    let positionPointOuVirgule = -1
 
-    // Mémoriser la nouvelle valeur de n, s'il y a lieu
     n = validerBasesEtFormaterNombre(n, b1, b2, nEstNegatif)
 
-    // Déclarer et initialiser les variables
+    if (!(n.includes(",") && n.includes(".")) && (n.includes(",") || n.includes("."))) {
+        if (n.includes(",")) {
+            positionPointOuVirgule = nPossedeUneVirgule(n, ",")
+        }
+
+        else {
+            positionPointOuVirgule = nPossedeUneVirgule(n, ".")
+        }
+    }
+
     const symboles = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9",
         "A", "B", "C", "D", "E", "F", "G", "H", "I", "J",
         "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T",
         "U", "V", "W", "X", "Y", "Z"]
 
-    // Mémoriser le résultat de la méthode validerNombreEtCalculerValeur(n, b1, symboles)
-    // Cette méthode converti n en une somme de valeurs
-    let valeurTotale = validerNombreEtCalculerValeur(n, b1, symboles)
+    let valeurTotale = validerNombreEtCalculerValeur(n, b1, symboles, positionPointOuVirgule)
 
-    // Mémoriser le résultat de la méthode calculerEtConvertir(valeurTotale, b2, symboles).
-    // Cette méthode converti valeurTotale en une combinaison symbole(s)
-    let resultatFinal = calculerValeurEtConvertirResultat(valeurTotale, b2, symboles)
+    let resultatFinal = calculerValeurEtConvertirResultat(valeurTotale, b2, symboles, positionPointOuVirgule)
 
     // Enlever la classe msgErreur à l'élément sélectionné, puis afficher le message suivant
     document.getElementById("message").classList.remove("msgErreur")
     document.getElementById("message").textContent = "Nombre converti avec succès"
+
+    if (n.includes(",")) {
+        resultatFinal = resultatFinal.replace(".", ",")
+    }
 
     // Si nEstNegatif === true, alors on ajoute un tiret en avant de resultatFinal,
     // puis on retourne le string ainsi formé.
@@ -83,40 +92,77 @@ function validerBasesEtFormaterNombre(nombre, b1, b2, nEstNegatif) {
     return nombre
 }
 
-// Méthode qui valide puis évalue la valeur de n
-function validerNombreEtCalculerValeur(n, b1, symboles) {
-    let valeur = 0
-    let exposant = 0
+function nPossedeUneVirgule(nombre, pointOuVirgule) {
+    let index = nombre.indexOf(pointOuVirgule)
 
-    // Pour chaque symbole présent dans nombre :
-    // 1) mémoriser temporairement le symbole dans une variable;
-    // 2) mémoriser le résultat de la méthode trouverSymbole(symbole, b1, symboles).
-    // Cette méthode valide, puis converti le symbole en nombre,
-    // ou retourne -1 si le symbole n'a pas été trouvé;
-    // 3) si valeurTemp === -1, alors on affiche des messages d'erreur.
-    // Dans les autres cas, on ne fait rien;
-    // 4) incrémenter la variable valeurTotale d'une valeur égale au résultat
-    // de valeurTemp * Math.pow(b1, exposant);
-    // 5) incrémenter la variable exposant de 1
-    for (let position = n.length - 1; position >= 0; position--)
-    {
-        let symbole = n[position]
-        let valeurTemp = trouverSymbole(symbole, b1, symboles)
-
-        if (valeurTemp === -1) {
-            document.getElementById("extrant").value = "Entrée invalide"
-            document.getElementById("message").classList.add("msgErreur")
-            document.getElementById("message").innerHTML = "Erreur : veuillez entrer un nombre entier, <br>" +
-                "représenté dans une base entière de 2 jusqu'à 36"
-
-            throw new Error("Le nombre entré contient des symboles illégaux")
-        }
-
-        valeur += valeurTemp * Math.pow(b1, exposant)
-        exposant++
+    if (index === nombre.lastIndexOf(pointOuVirgule)) {
+        return index
     }
 
-    return valeur
+    else {
+        document.getElementById("extrant").value = "Entrée invalide"
+        document.getElementById("message").classList.add("msgErreur")
+        document.getElementById("message").innerHTML = "Erreur : veuillez entrer un nombre entier, <br>" +
+            "représenté dans une base entière de 2 jusqu'à 36"
+        throw new Error()
+    }
+}
+
+// Méthode qui valide puis évalue la valeur de n
+function validerNombreEtCalculerValeur(n, b1, symboles, positionPointOuVirgule) {
+    if (positionPointOuVirgule !== -1) {
+        let valeur = 0
+        let exposant = positionPointOuVirgule + 1 - n.length
+
+        for (let position = n.length - 1; position >= 0; position--)
+        {
+            if (position === positionPointOuVirgule) {
+                continue;
+            }
+
+            let symbole = n[position]
+            let valeurTemp = trouverSymbole(symbole, b1, symboles)
+
+            if (valeurTemp === -1) {
+                document.getElementById("extrant").value = "Entrée invalide"
+                document.getElementById("message").classList.add("msgErreur")
+                document.getElementById("message").innerHTML = "Erreur : veuillez entrer un nombre entier, <br>" +
+                    "représenté dans une base entière de 2 jusqu'à 36"
+
+                throw new Error("Le nombre entré contient des symboles illégaux")
+            }
+
+            valeur += valeurTemp * Math.pow(b1, exposant)
+            exposant++
+        }
+
+        return valeur
+    }
+
+    else {
+        let valeur = 0
+        let exposant = 0
+
+        for (let position = n.length - 1; position >= 0; position--)
+        {
+            let symbole = n[position]
+            let valeurTemp = trouverSymbole(symbole, b1, symboles)
+
+            if (valeurTemp === -1) {
+                document.getElementById("extrant").value = "Entrée invalide"
+                document.getElementById("message").classList.add("msgErreur")
+                document.getElementById("message").innerHTML = "Erreur : veuillez entrer un nombre entier, <br>" +
+                    "représenté dans une base entière de 2 jusqu'à 36"
+
+                throw new Error("Le nombre entré contient des symboles illégaux")
+            }
+
+            valeur += valeurTemp * Math.pow(b1, exposant)
+            exposant++
+        }
+
+        return valeur
+    }
 }
 
 // Méthode qui compare les valeurs présentes dans le tableau symboles avec
@@ -135,24 +181,55 @@ function trouverSymbole(symbole, base1, symboles) {
 
 // Méthode qui converti la valeur fournie en un string qui représente
 // le résultat de la conversion
-function calculerValeurEtConvertirResultat(valeur, base2, symboles) {
+function calculerValeurEtConvertirResultat(valeur, base2, symboles, positionPointOuVirgule) {
     // Si valeur est égal à 0, retourner 0
     if (valeur === 0) {
         return "0"
     }
 
-    // Déclarer et initialiser une variable resultatTemp
-    let resultatTemp = ""
+    if (positionPointOuVirgule !== -1) {
+        let resultatTemp= ""
+        let tableau = valeur.toString().split(".")
+        let compteurPrecision = 0 // À cause de l'erreur machine
 
-    // Convertir la valeur en un string de symbole(s)
-    while (valeur > 0) {
-        // Déclarer et définir une variable residu à partir de la valeur % base2
-        let residu = valeur % base2
-        // Concaténer le symbole associé à symboles[residu] avec le(s) résultat(s) précédent(s)
-        resultatTemp = symboles[residu] + resultatTemp
-        // Mémoriser la valeur associé à Math.floor(valeur / base2) dans la variable valeur
-        valeur = Math.floor(valeur / base2)
+        tableau[1] = "0." + tableau[1]
+
+        while (tableau[1] > 0 && compteurPrecision < 14) {
+            let residu = Math.floor(tableau[1] * base2)
+            resultatTemp += symboles[residu].toString()
+            tableau[1] = tableau[1] * base2 - residu
+
+            compteurPrecision++
+        }
+
+        if (resultatTemp !== "") {
+            resultatTemp = "." + resultatTemp
+        }
+
+        if (tableau[0].toString() === "0" || tableau[0].toString() === "") {
+            resultatTemp = "0" + resultatTemp
+        }
+
+        while (tableau[0] > 0) {
+            let residu = tableau[0] % base2
+            resultatTemp = String(symboles[residu]) + resultatTemp
+            tableau[0] = Math.floor(tableau[0] / base2)
+        }
+
+
+        return resultatTemp
     }
 
-    return resultatTemp
+    else {
+        let resultatTemp = ""
+
+        while (valeur > 0) {
+            let residu = valeur % base2
+            resultatTemp = symboles[residu] + resultatTemp
+            valeur = Math.floor(valeur / base2)
+        }
+
+        return resultatTemp
+    }
+
 }
